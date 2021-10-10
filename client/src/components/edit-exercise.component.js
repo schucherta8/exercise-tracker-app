836dac5d3dee0ad2d621efd1046e1ef871711eb7
schucherta8 <andres.schuchert@gmail.com>
2021-10-09 21:19:18 -0400

    Created edit-exercise and exercises-list components. Edit-exercise is simpilar to create-exercise component, while exercises-list is displayed as a table. Used Axios POST and GET Requests to get information from the database.

--- client/src/components/edit-exercise.component.js
+++ client/src/components/edit-exercise.component.js
@@ -1,14 +1,125 @@
 import React, { Component } from 'react';
+import DatePicker from 'react-datepicker';
+import axios from 'axios';
+import "react-datepicker/dist/react-datepicker.css";
 
 export default class EditExercise extends Component {
 	constructor(props) {
 		super(props);
+		this.state = {
+			username: '',
+			description: '',
+			duration: 0,
+			date: new Date(),
+			users: [],
+		};
+		this.onChange = this.onChange.bind(this);
+		this.onDate = this.onDate.bind(this);
+		this.onSubmit = this.onSubmit.bind(this);
 	}
 
+	componentDidMount() {
+		axios.get("http://localhost:5000/api/v1/exercises/" + this.props.match.params.id)
+		.then(res => {
+			this.setState({
+				username: res.data.username,
+				description: res.data.description,
+				duration: res.data.duration,
+				date: new Date(res.data.date)
+			});
+		})
+		.catch(err => console.log(err));
+
+		axios.get("http://localhost:5000/api/v1/users/")
+		.then(res => {
+			if(res.data.length > 0) {
+				this.setState({
+					users: res.data.map(user => user.username),
+				})
+			}
+		})
+		.catch(err => console.log(err));
+	}
+
+	onChange(e) {
+		this.setState({[e.target.name]: e.target.value});
+	}
+
+	onDate(date) {
+		this.setState({date: date});
+	}
+
+	onSubmit(e) {
+		e.preventDefault();
+
+		const exercise = {
+			username: this.state.username,
+			description: this.state.description,
+			duration: this.state.duration,
+			date: this.state.date,
+		};
+
+		axios.post('http://localhost:5000/api/v1/exercises/update/' + this.props.match.params.id, exercise)
+		.then(res => {
+			console.log(res.data)
+			window.location = '/';
+		});
+	}
 	render() {
 		return (
 			<div>
-				<p>You are on the Edit Exercise component!</p>
+				<h3>Edit Exercise Log</h3>
+				<form onSubmit={this.onSubmit}>
+					<div className="form-group">
+						<label>Username: </label>
+						<select 
+							ref="userInput"
+							required
+							className="form-control"
+							name="username"
+							value={this.state.username}
+							onChange={this.onChange}>
+							{
+								this.state.users.map(user => 
+								<option
+									key={user}
+									value={user}>
+									{user}
+								</option>)
+							}	
+						</select>
+					</div>
+					<div className="form-group">
+						<label>Description: </label>
+						<input
+							type="text"
+							name="description"
+							required
+							className="form-control"
+							value={this.state.description}
+							onChange={this.onChange} />
+					</div>
+					<div className="form-group">
+						<label>Duration (in minutes): </label>
+						<input
+							type="text"
+							name="duration"
+							required
+							className="form-control"
+							value={this.state.duration}
+							onChange={this.onChange} />
+					</div>
+					<div className="form-group">
+						<label>Date: </label>
+						<DatePicker
+							selected={this.state.date}
+							onChange={this.onDate}
+						/>
+					</div>
+					<div className="form-group">
+						<input type="submit" value="Edit Exercise Log" className="btn btn-primary" />
+					</div>
+				</form>
 			</div>
 		)
 	}
--- client/src/components/exercises-list.component.js
+++ client/src/components/exercises-list.component.js
@@ -1,14 +1,81 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+
+const Exercise = props => (
+	<tr>
+		<td>{props.exercise.username}</td>
+		<td>{props.exercise.description}</td>
+		<td>{props.exercise.duration}</td>
+		<td>{props.exercise.date.substring(0,10)}</td>
+		<td>
+			<Link to={"/edit/"+props.exercise._id}>edit</Link> | 
+			<button className="btn btn-primary" onClick={() => 
+				{props.deleteExercise(props.exercise._id)}}>Delete</button>
+			{/* <a href="#" onClick={() => 
+				{props.deleteExercise(props.exercise._id)}}>Delete</a> */}
+		</td>
+	</tr>
+);
 
 export default class ExercisesList extends Component {
 	constructor(props) {
 		super(props);
+		this.state = {
+			exercises: [],
+		};
+		this.deleteExercise = this.deleteExercise.bind(this);
+	}
+
+	componentDidMount() {
+		axios.get("http://localhost:5000/api/v1/exercises/")
+		.then(res => {
+			this.setState({exercises: res.data});
+		})
+		.catch(err => console.log(err));
+	}
+
+	deleteExercise(id) {
+		axios.delete("http://localhost:5000/api/v1/exercises/"+id)
+		.then(res => {
+			console.log(res.data);
+		});
+
+		this.setState({
+			exercises: this.state.exercises.filter(exercise => 
+			exercise._id !== id)
+		});
+	}
+	
+	exerciseList() {
+		return
 	}
 
 	render() {
 		return (
 			<div>
-				<p>You are on the Exercise List component!</p>
+				<h3>Logged Exercises</h3>
+				<table className="table">
+					<thead className="thead-light">
+						<tr>
+							<th>Username</th>
+							<th>Description</th>
+							<th>Duration</th>
+							<th>Date</th>
+							<th>Actions</th>
+						</tr>
+					</thead>
+					<tbody>
+						{
+							this.state.exercises.map(exercise => 
+							<Exercise 
+								exercise={exercise} 
+								deleteExercise={this.deleteExercise}
+								key={exercise._id}
+							/>)
+						}
+					</tbody>
+				</table>
 			</div>
 		)
 	}
